b8daf85a6134b8a601f55b187e9e277df289f357
Victor <Victor.aragremio@gmail.com>
2019-03-13 20:10:03 -0300

    Add files via upload
    Exercicio Prog1

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main{
+   public static void main(String args[]){
+     Pessoa a,b,c;
+     a=new Pessoa();
+     b=new Pessoa();
+     c=new Pessoa();
+     a.idade=22;
+     b.altura=1.78;
+     c.anoNascimento=1996;
+     System.out.println("Idade:"+a.idade+"\n"+"altura:"+b.altura+"\n"+"Ano de Nascimento:"+c.anoNascimento+"\n");
+}
+}
